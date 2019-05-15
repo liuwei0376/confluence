c4400a9f74b572bbacecc043a1b1a9a5ddb21544
LiuWei0376 <liuwei_0376@163.com>
2019-05-15 16:04:48 +0800

    Update keygen.bat
    add jdk8 support

--- crackfile/iNViSiBLE/keygen.bat
+++ crackfile/iNViSiBLE/keygen.bat
@@ -1,28 +1,28 @@
 @echo off
 
-set JAVA6="auto"
+set JAVA8="auto"
 set JAR=confluence_keygen.jar
 
-set X86="%ProgramFiles(x86)%"
-if %JAVA6% == "auto" (
+set X86="%ProgramFiles%"
+if %JAVA8% == "auto" (
   echo Autodetecting java
   if %X86% NEQ "" (
     echo Testing x86 folder
-    for /D %%j in ("%ProgramFiles(x86)%\jre6*" "%ProgramFiles(x86)%\jre1.6.*" "%ProgramFiles(x86)%\jdk1.6.*" "%ProgramFiles(x86)%\Java\jre6*" "%ProgramFiles(x86)%\Java\jre1.6.*" "%ProgramFiles(x86)%\Java\jdk1.6.*") do (
+    for /D %%j in ("%ProgramFiles%\jre8*" "%ProgramFiles%\jre1.8.*" "%ProgramFiles%\jdk1.8.*" "%ProgramFiles%\Java\jre8*" "%ProgramFiles%\Java\jre1.8.*" "%ProgramFiles%\Java\jdk1.8.*") do (
       echo Checking %%j folder
       if exist "%%j\bin\java.exe" (
         echo Found java in %%j folder
-        set JAVA6="%%j\bin\java.exe"
+        set JAVA8="%%j\bin\java.exe"
       )
     )
   ) else (
     echo Testing default folder
     if defined ProgramFiles (
-    for /D %%j in ("%ProgramFiles%\jre6*" "%ProgramFiles%\jre1.6.*" "%ProgramFiles%\jdk1.6.*" "%ProgramFiles%\Java\jre6*" "%ProgramFiles%\Java\jre1.6.*" "%ProgramFiles%\Java\jdk1.6.*") do (
+    for /D %%j in ("%ProgramFiles%\jre8*" "%ProgramFiles%\jre1.8.*" "%ProgramFiles%\jdk1.8.*" "%ProgramFiles%\Java\jre8*" "%ProgramFiles%\Java\jre1.8.*" "%ProgramFiles%\Java\jdk1.8.*") do (
         echo Checking %%j folder
         if exist "%%j\bin\java.exe" (
           echo Found java in %%j folder
-          set JAVA6="%%j\bin\java.exe"
+          set JAVA8="%%j\bin\java.exe"
         )
       )
     )
@@ -30,14 +30,14 @@ if %JAVA6% == "auto" (
 )
 
 :rerun
-if %JAVA6% == "auto" (
+if %JAVA8% == "auto" (
   echo ERROR: Java not found!
   echo Please download and install from java.com
   pause
   exit
 ) else (
-  echo Starting java from %JAVA6%
-  %JAVA6% -jar %JAR%
+  echo Starting java from %JAVA8%
+  %JAVA8% -jar %JAR%
 
   if errorlevel 11 (
     if not exist %JAR% (
@@ -47,7 +47,7 @@ if %JAVA6% == "auto" (
     )
 
     echo ERROR: Failed to run %JAR%
-    echo JAVA6=%JAVA6%
+    echo JAVA8=%JAVA8%
     pause
     exit
   )
@@ -64,7 +64,7 @@ if %JAVA6% == "auto" (
     )
 
     echo ERROR: Failed to run %JAR%
-    echo JAVA6=%JAVA6%
+    echo JAVA8=%JAVA8%
     pause
     exit
   )
